eb1e71a8ad3ae7ae74579ab6b918a09d3cf07fde
George_Lewis <georgejlewis@hotmail.com>
2018-03-29 19:52:54 +0200

    Testing the sad path

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,7 +2,38 @@
 
 require './lib/atm.rb'
 describe Atm do
+  let(:account) { instance_double('Account') }
+  before do
+    # Before each test we need to add an attribute of 'balance'
+    # to the 'account' object and set the value to '100'
+    allow(account).to receive(:balance).and_return(100)
+    # We also need to allow 'account' to receive the new balance
+    # using the setter method 'balance='
+    allow(account).to receive(:balance=)
+  end
+
   it 'has 1000$ on intitialize' do
     expect(subject.funds).to eq 1000
+  end
+
+  it 'funds are reduced at withdraw' do
+    subject.withdraw(50, account)
+    expect(subject.funds).to eq 950
+  end
+
+  it 'allow withdraw if account has enough balance.' do
+  expected_output = { status: true, message: 'success', date: Date.today, amount: 45}
+  expect(subject.withdraw(45, account)).to eq expected_output
+  end
+
+  it 'rejects withdraw if account has insufficient funds' do
+    expected_output = { status: false, message: 'insufficient funds in account', date: Date.today}
+    expect(subject.withdraw(105, account)).to eq expected_output
+  end
+
+  it 'rejects withdraw if ATM has insufficient funds' do
+    subject.funds = 50
+    expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
+    expect(subject.withdraw(100, account)).to eq expected_output
   end
 end
