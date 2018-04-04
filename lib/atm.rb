0fae23544914b7c544ab5a522fd43ba08ee0de06
George_Lewis <georgejlewis@hotmail.com>
2018-04-04 16:06:52 +0200

    Added different bill denominations

--- lib/atm.rb
+++ lib/atm.rb
@@ -26,6 +26,18 @@ class Atm
 
   private
 
+    def add_bills(amount)
+      denominations = [20, 10, 5]
+      bills = []
+      denominations.each do |bill|
+        while amount - bill >= 0
+          amount -= bill
+          bills << bill
+        end
+      end
+      bills
+    end
+
     def card_expired?(exp_date)
       Date.strptime(exp_date, '%m/%y') < Date.today
     end
@@ -45,7 +57,7 @@ class Atm
     def perform_transaction(amount, account)
       @funds -= amount
       account.balance = account.balance - amount
-      { status: true, message: 'success', date: Date.today, amount: amount }
+      { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount) }
     end
 
   end
