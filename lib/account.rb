56e9221f4ce73ac582de1df1f3f75641fc2f828f
George_Lewis <georgejlewis@hotmail.com>
2018-04-05 11:47:30 +0200

    added expiary date also account status but account status keeps returning Nil

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,22 @@
 #account.rb
-
+require 'date'
 class Account
-  attr_accessor :balance
+  attr_accessor :balance, :account_status,
+  STANDARD_VALIDITY_YRS = 5
 
-  def intitialize(balance)
+  def intitialize(attrs = {})
     @balance = balance
     @pin_code = pin_creation
+    @expiary_date = exp_date
+    @account_status = :active
   end
 
   def pin_creation
     rand(1000...9999)
   end
+
+  def exp_date
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,4 +10,13 @@ describe Account do
     expect(number_length)
   end
 
+  it 'is expected to have expiary date on intitialize' do
+    expected_date = Date.today.next_year(5).strftime('%m/%y')
+    expect(subject.exp_date).to eq expected_date
+  end
+
+  it 'is expected to have :active status on intitialize' do
+    expect(subject.account_status).to eq :active
+  end
+
 end
